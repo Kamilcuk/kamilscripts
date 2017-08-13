e1c24b616ebfbb4208a4e32f318f234e129cb9c0
Kamil <kamilcukrowski@gmail.com>
2017-08-13 23:50:36 +0200

    Update qqnetcattarpipe.sh

--- public/resources/usr/bin/qqnetcattarpipe.sh
+++ public/resources/usr/bin/qqnetcattarpipe.sh
@@ -1,38 +1,103 @@
 #!/bin/bash
+set -euo pipefail
+
+# functions ################################################### 
 
 usage() {
-        echo "$0 [-s] out   <folder/pliki> <host> <port>"
-	echo "$0 [-s] in    <folder fo wywalenia> <port>"
-	echo "$0 [-s] inraw <plik do wywalenia>   <port>"
-        echo "This program makes netcat tar pipe."
-	echo " -s = nc is silent."
-        exit 0
+	cat <<EOF
+Usage:  
+	netcattarpipe.sh [OPTIONS] out <hostname> <port> <source>...
+		Compresses sources using tar+gzip pipe and netcats it to hostname:port
+	netcattarpipe.sh [OPTIONS] in  <port> <destination>
+		Receives compressed stream on port and uncompresses it to destination directory.
+	netcattarpipe.sh [OPTIONS] inraw <port> <destination>
+		Receiving compressed stream on port and saves it to destination.
+
+Options:
+	-s 	- Don't pass verbose flag -v to tar and netcat.
+	-h 	- Print this help and exit.
+	-t OPT	- Pass additional option OPT to tar command. This option is stacked.
+	-n OPT	- Pass additional option OPT to nc command. This option is stacked.
+
+Example:
+	For ex. to copy files from directory /a/b on host1 to directory /c/b/ on host2:
+	First on host2 run 'netcattarpipe.sh in 10000 /c/b/' , then
+	on host1 run 'netcattarpipe.sh out host2 10000 /a/b/'
+
+Written by Kamil Cukrowski 2017. Under MIT license.
+EOF
+	exit 1
 }
-test $# -lt 1 && usage
-OPTS='-v'
-if test ${1} == '-s'; then
-	OPTS='';
-	shift;
+
+error() { echo "$@" >&2; }
+
+test() {
+	$0 -h || true
+	temp=$(mktemp -d)
+	temp2=$(mktemp -d)
+	trap 'rm -rf $temp $temp2' EXIT
+	echo 123 > $temp/123
+	$0 -s -t -v -n -v in 10000 $temp2 &
+	$0 -s -t -v -n -v out localhost 10000 $temp
+	wait
+	if [ "$(cat $temp2/123)" != "123" ]; then
+		error 'Error if [ "$(cat $temp2/$(basename $temp)/123)" != "123" ]; then'
+	fi
+	echo "test end"
+	exit 0
+}
+
+# input ################################################
+
+if [ $# -lt 1 ]; then usage; fi
+verbose=true
+taropts=()
+ncopts=()
+while getopts ':sht:n:' opt; do
+	case "$opt" in
+	s) verbose=false ;;
+	h) usage ;;
+	t) taropts+=( "$OPTARG" ) ;;
+	n) ncopts+=( "$OPTARG" ) ;;
+	\?) error "Invalid option: -$OPTARG"; usage; exit 1; ;;
+	:) error "Option -$OPTARG requires an argument."; usage; exit 1;  ;;
+	esac
+done
+shift $((OPTIND-1))
+
+if $verbose; then
+	taropts+=( '-v' )
+	ncopts+=( '-v' )
 fi
 
-case ${1} in
-"out")
-	test $# -lt 4 && usage
-	echo "+ tar ${OPTS} -cf - $(readlink -f ${2}) | nc ${OPTS} -4 ${3} ${4}"
-	tar ${OPTS} -cf - $(readlink -f ${2}) | nc ${OPTS} -4 ${3} ${4}
-	;;
-"inraw")
-        test $# -lt 2 && usage
-	echo "+ nc -4 ${OPTS} -l ${3} > $(readlink -f ${2})"
-        nc -4 ${OPTS} -l ${3} > "$(readlink -f ${2})"
-        ;;
+# main ###################################################
 
-"in")
-	test $# -lt 2 && usage
-	echo "+ nc -4 ${OPTS} -l ${3} | tar ${OPTS} -xpf - -C $(readlink -f ${2})"
-	nc -4 ${OPTS} -l ${3} | tar ${OPTS} -xpf - -C "$(readlink -f ${2})"
+if [ $# -lt 3 ]; then error "Not enough arguments."; usage; fi
+case "$1" in
+out)
+	if [ $# -lt 4 ]; then error "Not enough arguments."; usage; fi
+	ncopts+=( "$2" "$3" )
+	shift 3
+	set -x
+	tar "${taropts[@]}" -z -c -f - "$@" | nc -4 "${ncopts[@]}"
 	;;
-*) usage ;;
+inraw)
+	OUT=$(readlink -f "$3")
+	set -x;
+	nc -4 "${taropts[@]}" -l "$2" > "$OUT"
+	;;
+in)
+	OUT=$(readlink -f "$3")
+	set -x;
+	nc -4 "${ncopts[@]}" -l "$2" | tar "${taropts[@]}" -z -x -f - -C "$OUT";
+	;;
+test)
+	test 
+	;;
+*)
+	error "Wrong mode used. Mode must be one of out, inraw or in."
+	usage
+	;;
 esac
 
 
