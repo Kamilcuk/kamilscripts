ba0ec2f5ded131aa67dda7d8cea9c95b4d900225
Kamil Cukrowski <kamilcukrowski@gmail.com>
2019-05-22 00:14:25 +0200

    Disable artifacts pasisng to push notify and fix artifacts

--- pkg/archlinux/docker_build.sh
+++ pkg/archlinux/docker_build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -xeuo pipefail
+
 pacman -Sy --needed --noconfirm base-devel git sudo
 chown -R nobody:nobody .
 chmod +w /etc/sudoers
 printf "nobody ALL=(ALL) NOPASSWD:"" ALL" >> /etc/sudoers
 chmod -w /etc/sudoers
 
-cd pkg/archlinux
+pushd pkg/archlinux >/dev/null
 sudo -u nobody makepkg -f --syncdeps --noconfirm
 
 ls -l *.pkg.tar.xz
 
+popd
 if (($# > 0)); then
 	mkdir -v -p "$1"
 	mv -v *.pkg.tar.xz "$1"/
