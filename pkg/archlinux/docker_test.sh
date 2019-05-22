1eb57a347a1c0d5fc6a8ad2b0dc40416fdbd9c51
Kamil Cukrowski <kamilcukrowski@gmail.com>
2019-05-22 12:36:03 +0200

    Fix repo creation

--- pkg/archlinux/docker_test.sh
+++ pkg/archlinux/docker_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
-docker run -v $(git rev-parse --show-toplevel):/repo:ro archlinux/base sh -c '
+docker run -v $(git rev-parse --show-toplevel):/repo:ro \
+	$(<<<archlinux/base :) antergos/makepkg sh -c '
 cp -a /repo /work && 
 cd /work &&
 pkg/archlinux/docker_build.sh /output
--- pkg/archlinux/repo_create.sh
+++ pkg/archlinux/repo_create.sh
@@ -4,5 +4,6 @@ repo="$1"
 pkgsdir="$2"
 pkgs=$(find "$pkgsdir" -type f -name '*.pkg.tar.xz')
 mkdir -p $(dirname "$repo")
+cp -va $pkgs "$(dirname "$repo")"
 repo-add -n "$repo".db.tar.xz $pkgs
 
