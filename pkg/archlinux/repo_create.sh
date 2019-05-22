e1ddb29e788543e8a40f329dc9fe0161125048ae
Kamil Cukrowski <kamilcukrowski@gmail.com>
2019-05-22 10:32:28 +0200

    Fix repo generation scripts #3

--- pkg/archlinux/repo_create.sh
+++ pkg/archlinux/repo_create.sh
@@ -2,7 +2,7 @@
 set -xeuo pipefail
 repo="$1"
 pkgsdir="$2"
-pkgs=$(find "$pkgdir" -type f -name '*.pkg.tar.xz')
+pkgs=$(find "$pkgsdir" -type f -name '*.pkg.tar.xz')
 mkdir -p $(dirname "$repo")
 repo-add "$repo".db.tar.xz $pkgs
 
