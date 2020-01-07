e42e0f9b38d4078c5773b498cad55834ab1d12ac
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-01-07 15:32:24 +0100

    fix(kamil_edit): Handle git@gitlab remove properly

--- kamil_edit.sh
+++ kamil_edit.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -xuoe pipefail
+
 if ! tmp=$(git remote -v | head -n1 | awk '{print $2}'); then
 	echo "ERROR: problem getting git remote" >&2
 	exit 1
 fi
-if [ "$tmp" != "https://gitlab.com/Kamcuk/kamilscripts.git" ]; then
+
+case "$tmp" in
+"https://gitlab.com/Kamcuk/kamilscripts.git") ;;
+"git@gitlab.com:Kamcuk/kamilscripts.git") ;;
+*)
 	echo "ERROR: wrong git remote $tmp - bailing out" >&2
 	exit 1
-fi
+esac
+
 cd "$(dirname "$(readlink -f "$0")")"
 if [ "$(pwd)" != /usr/lib/kamilscripts ]; then
 	echo "ERROR: not in instalation dir" >&2
