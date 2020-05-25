88fa509efe94bfbfab7dac816ada717f4ad22f36
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-05-25 10:20:59 +0200

    fix(bash): alias_complete speedup and cleanup

--- bin/alias_complete.sh
+++ bin/alias_complete.sh
@@ -1,32 +1,16 @@
 #!/bin/bash
 
-# allow sourcing
-if [[ "${BASH_SOURCE[0]}" != "${0}" && "$_" != "$0" ]]; then
-	if (($# < 2)); then
-		echo "alias_complete: Invalid number of arguments" >&2
-		return 2
-	fi
-	if _alias_complete_tmp=$("${BASH_SOURCE[0]}" "$@"); then
-		eval "$_alias_complete_tmp"
-		unset _alias_complete_tmp
-	fi
-	return $?
-fi
+# Private Functions ##########################################################
 
-set -euo pipefail
-shopt -s extglob
-
-# Functions ##########################################################
-
-usage() {
+_aLiAs_CoMpLeTe__usage() {
       cat <<'EOF'
 Usage: 
 	alias_complete.sh [options] name command [args...]
-	eval "$(alias_complete.sh name command [args])"
+	source alias_complete.sh name command [args]
 
 Aliases autocompletion scripts of alias with the name "name"
 to command "command [args...]".
-Tries to handle all arguments and COMP_* vairables.
+Tries to handle all arguments and COMP_* variables.
 
 Options:
 	-h --help        - Print this help and exit.
@@ -34,265 +18,357 @@ Options:
 	--longopt        - Alias this command to _longopt autocompletion.
 	--shortopt       - Alias this command to _shortopt autocompletion.
 	-s --silent      - Silence the output. Don't show any output except success.
+	   --unittest    - Test the script
 
 Usage examples:
 	- alias l='ls -la'
-	  eval "$(alias_complete.sh l ls)
+	  source alias_complete.sh l ls
 	- alias pS='pacman -S --noconfirm'
-	  eval "$(alias_complete.sh pS pacman -S --noconfirm)"
+	  source alias_complete.sh pS pacman -S --noconfirm
 	- Or just:
-	  eval "$(alias_complete.sh -a pS pacman -S --noconfirm)"
+	  source alias_complete.sh -a pS pacman -S --noconfirm
+
+WARNING:
+  This script declares symbols available at parent scope.
+  It is sourced for _speed_.
+  All external symbols that should persist after script returns are 
+  	only 3 or 4 functions inside _aLiAs_CoMpLeTe_::* namespace.
+  All private symbols that should be unset before this script returns are within
+  	_aLiAs_CoMpLeTe__* namespace.
+  The script sets shopt -s extglob
+
 
 Written by Kamil Cukrowski
 Licensed jointly under MIT License and Beerware License.
 EOF
 }
 
-fatal() {
-	declare -g silent
-	if ! "$silent"; then
+_aLiAs_CoMpLeTe__clearenv() {
+	# shellcheck disable=SC2154
+	unset "${!_aLiAs_CoMpLeTe__@}"
+	# shellcheck disable=SC2046
+	unset -f $(declare -F | sed -n '/_aLiAs_CoMpLeTe__/s/^declare -f //p')
+}
+
+_aLiAs_CoMpLeTe__fatal() {
+	declare -g _aLiAs_CoMpLeTe__silent
+	if ! "$_aLiAs_CoMpLeTe__silent"; then
 		echo "alias_complete.sh: ERROR:" "$@" >&2
 	fi
-	exit 2
+	_aLiAs_CoMpLeTe__clearenv
+}
+
+_aLiAs_CoMpLeTe__run_unittests() {
+	local err
+	err=false
+
+	err() {
+		err=true
+		echo "alias_complete.sh: ERROR: $1" >&2
+	}
+	assert() {
+		if ! eval "$2"; then
+			err "$1"
+		fi
+	}
+	run() {
+		bash -c 'set -euo pipefail -o errexit; '"$1"
+	}
+
+	local tmp
+
+	# shellcheck disable=SC2016
+	tmp=$(run 'source alias_complete.sh l ls; echo "${!_aLiAs_CoMpLeTe__@}"')
+	assert "$LINENO" "(($? == 0))"
+	# shellcheck disable=SC2016
+	assert "$LINENO" '[[ -z "$tmp" ]]'
+
+	# shellcheck disable=SC2016
+	tmp=$(run 'source alias_complete.sh l ls;
+			echo $(declare -F | sed -n '\''/_aLiAs_CoMpLeTe__/s/^declare -f //p'\'')')
+	assert "$LINENO" "(($? == 0))"
+	# shellcheck disable=SC2016
+	assert "$LINENO" '[[ -z "$tmp" ]]'
+
+	# shellcheck disable=SC2016
+	tmp=$(run 'source alias_complete.sh l ls; 
+		declare -F | sed -n '\''/_aLiAs_CoMpLeTe_/s/^declare -f //p'\''')
+	assert "$LINENO" "(($? == 0))"
+	# shellcheck disable=SC2016
+	assert "$LINENO" '[[ -n "$tmp" ]]'
+
+	if ! "$err"; then
+		echo "SUCCESS"
+	fi
 }
 
 # Parse arguments ###########################################################
 
-shortopt=false
-longopt=false
-withalias=false
-silent=false
+_aLiAs_CoMpLeTe__shortopt=false
+_aLiAs_CoMpLeTe__longopt=false
+_aLiAs_CoMpLeTe__withalias=false
+_aLiAs_CoMpLeTe__silent=false
+_aLiAs_CoMpLeTe__unittest=false
 while (($#)); do
 	case "$1" in
-	-h|--help) usage; exit 0; ;;
-	-a|--with-alias) withalias=true; ;;
-	--longopt) longopt=true; ;;
-	--shortopt) shortopt=true; ;;
-	-s|--silent) silent=true; ;;
+	-h|--help) _aLiAs_CoMpLeTe__usage; exit 0; ;;
+	-a|--with-alias) _aLiAs_CoMpLeTe__withalias=true; ;;
+	--longopt) _aLiAs_CoMpLeTe__longopt=true; ;;
+	--shortopt) _aLiAs_CoMpLeTe__shortopt=true; ;;
+	-s|--silent) _aLiAs_CoMpLeTe__silent=true; ;;
+	--unittest) _aLiAs_CoMpLeTe__unittest=true; ;;
 	--) shift; break; ;;
-	-*) fatal "Unknown option: $1"; ;;
+	-*) _aLiAs_CoMpLeTe__fatal "Unknown option: $1"; return 2; ;;
 	*) break; ;;
 	esac
 	shift
 done
 
-if (($# < 1)); then
-	usage >&2
-	fatal "No alias name specified"
-fi
-
-alias_name="$1"
-if [[ -z "$alias_name" ]]; then
-	fatal "First argument is empty"
-fi
-
-if "$longopt" && "$shortopt"; then
-	fatal "The --shortopt and --longopt cannot be used together. --shortopt includes --longopt"
-fi
-if ( "$longopt" || "$shortopt" ) && "$withalias"; then
-	fatal "The --with-alias must be used without --shortopt not --longopt. To which command shoud we alias?"
-fi
-
-# handle longopt
-if "$longopt"; then
-	if (($# > 1)); then
-		fatal "The --longopt options uses only alias name."
-	fi
-	cat <<'EOF'
-_alias_complete::longopt() {
-	if [[ -z "${BASH_COMPLETION_VERSINFO:-}" ]]; then
-		. "/usr/share/bash-completion/bash_completion"
-	fi
-	_longopt "$@"
-}
-EOF
-	cat <<EOF
-complete -F _alias_complete_longopt "$alias_name"
-EOF
-	exit
-fi
-
-# handle shortopt
-if "$shortopt"; then
-	if (($# > 1)); then
-		fatal "The --shortopt options uses only alias name"
-	fi
-
-_alias_complete::shortopt() {
-        if [[ -z "${BASH_COMPLETION_VERSINFO:-}" ]]; then
-	     . "/usr/share/bash-completion/bash_completion"
-	fi
-
-    local cur prev words cword split
-    _init_completion -s || return
-
-    case "${prev,,}" in
-        --help|--usage|--version)
-            return
-            ;;
-        --!(no-*)dir*)
-            _filedir -d
-            return
-            ;;
-        --!(no-*)@(file|path)*)
-            _filedir
-            return
-            ;;
-        --+([-a-z0-9_]))
-            local argtype=$(LC_ALL=C $1 --help 2>&1 |
-		    command sed -ne "s|.*$prev\[\{0,1\}=[<[]\{0,1\}\([-A-Za-z0-9_]\{1,\}\).*|\1|p")
-            case ${argtype,,} in
-                *dir*)
-                    _filedir -d
-                    return
-                    ;;
-                *file*|*path*)
-                    _filedir
-                    return
-                    ;;
-            esac
-            ;;
-    esac
-
-    $split && return
-
-    COMPREPLY=( $(compgen -W "$(
-         LC_ALL=C "$1" --help 2>&1 |
-         command tr ' ' '\n' |
-         command grep -x -- '-[-A-Za-z0-9]\+\(=.*\)\?' |
-         command cut -d= -f2- |
-         command sort -u
-    )" -- "$cur") )
-}
-
-	cat <<EOF
-shopt -s extglob
-$(declare -f _alias_complete::shortopt)
-complete -F _alias_complete::shortopt "$alias_name"
-EOF
-	exit
-fi
-
-# parse rest of options
-
-if (($# < 2)); then
-	fatal "Command argument exprected"
-fi
-cmd="$2"
-if [[ -z "$cmd" ]]; then
-	fatal "Command is empty"
-fi
-shift 2
-cmd_args=("$@")
-
-if [[ "$cmd" =~ *[[:space:]]* ]]; then
-	fatal "The script does not work with commands with spaces in it."
-fi
-
-# Do the normal completion
-
-output=$(
-
-if "$withalias"; then
-	cmd_with_args_escaped=$(printf " %q" "$cmd" "${cmd_args[@]}" | cut -c2-)
-	echo "alias $alias_name='$cmd_with_args_escaped'"
-fi
-
-_complete_alias::do::alias() {
-	if (($# < 2)); then
-		echo "complete_alias::do::alias: ERROR: need more then 2 arguments" >&2
+if "$_aLiAs_CoMpLeTe__unittest"; then
+	if [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
+		_aLiAs_CoMpLeTe__fatal "For unittests to run, do not source this script"
 		return 2
 	fi
+	_aLiAs_CoMpLeTe__run_unittests
+	exit "$?"
+fi
 
-	local alias_name cmd cmd_args
-	alias_name=$1
-	cmd=$2
-	shift 2
-	cmd_args=("$@")
+# We need to be sourced
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+	_aLiAs_CoMpLeTe__fatal "This script has to be sourced"
+	exit 2
+fi
 
-	local tmp
+if (($# < 1)); then
+	_aLiAs_CoMpLeTe__usage >&2
+	_aLiAs_CoMpLeTe__fatal "No alias name specified"
+	return 2
+fi
+if [[ -z "$1" ]]; then
+	_aLiAs_CoMpLeTe__fatal "First argument is empty"
+	return 2
+fi
+if "$_aLiAs_CoMpLeTe__longopt" && "$_aLiAs_CoMpLeTe__shortopt"; then
+	_aLiAs_CoMpLeTe__fatal "The --shortopt and --longopt cannot be used together. --shortopt includes --longopt"
+	return 2
+fi
+if ( "$_aLiAs_CoMpLeTe__longopt" || "$_aLiAs_CoMpLeTe__shortopt" ) && 
+		"$_aLiAs_CoMpLeTe__withalias"; then
+	_aLiAs_CoMpLeTe__fatal "The --with-alias must be used without --shortopt not --longopt. To which command shoud we alias?"
+	return 2
+fi
+
+# Exported functions #################################################
+
+shopt -s extglob
+
+_aLiAs_CoMpLeTe_::shortopt() {
 	if [[ -z "${BASH_COMPLETION_VERSINFO:-}" ]]; then
-		# echo _complete_alias::do::alias sourcing /usr/share/bash-completion/bash_completion
+		# shelcheck disable=SC1091
 		if ! . "/usr/share/bash-completion/bash_completion"; then
 			echo "complete_alias: Sourcing /usr/share/bash-completion/bash_completion failed" >&2
 			return 2
 		fi
 	fi
+
+	# shellcheck disable=SC2034
+    local cur prev words cword split
+    _init_completion -s || return
+
+	case "${prev,,}" in
+    	--help|--usage|--version)
+        	return
+            ;;
+    	--!(no-*)dir*)
+        	_filedir -d
+        	return
+            ;;
+    	--!(no-*)@(file|path)*)
+        	_filedir
+        	return
+            ;;
+    	--+([-a-z0-9_]))
+			local argtype
+        	argtype=$(LC_ALL=C $1 --help 2>&1 |
+	    	command sed -ne "s|.*$prev\[\{0,1\}=[<[]\{0,1\}\([-A-Za-z0-9_]\{1,\}\).*|\1|p")
+        	case ${argtype,,} in
+            	*dir*)
+                	_filedir -d
+                	return
+                    ;;
+            	*file*|*path*)
+                	_filedir
+                	return
+                    ;;
+        	esac
+        	;;
+    esac
+
+    $split && return
+
+    # shellcheck disable=SC2207
+	COMPREPLY=( $(compgen -W "$(
+     	LC_ALL=C "$1" --help 2>&1 |
+     	command tr ' ' '\n' |
+     	command grep -x -- '-[-A-Za-z0-9]\+\(=.*\)\?' |
+     	command cut -d= -f2- |
+     	command sort -u
+    )" -- "$cur") )
+}
+
+_aLiAs_CoMpLeTe_::longopt() {
+	if [[ -z "${BASH_COMPLETION_VERSINFO:-}" ]]; then
+		# shelcheck disable=SC1091
+		if ! . "/usr/share/bash-completion/bash_completion"; then
+			echo "alias_complete: Sourcing /usr/share/bash-completion/bash_completion failed" >&2
+			return 2
+		fi
+	fi
+	_longopt "$@"
+}
+
+_aLiAs_CoMpLeTe_::do::alias() {
+	if (($# < 2)); then
+		echo "alias_complete::do::alias: ERROR: need more then 2 arguments" >&2
+		return 2
+	fi
+
+	local alias_name
+	alias_name=$1
+	shift
+	local cmd
+	cmd=$1
+	local cmd_with_args
+	cmd_with_args=("$@")
+
+	if [[ -z "${BASH_COMPLETION_VERSINFO:-}" ]]; then
+		# echo _aLiAs_CoMpLeTe_::do::alias sourcing /usr/share/bash-completion/bash_completion
+		# shelcheck disable=SC1091
+		if ! . "/usr/share/bash-completion/bash_completion"; then
+			echo "alias_complete: Sourcing /usr/share/bash-completion/bash_completion failed" >&2
+			return 2
+		fi
+	fi
+
+	local tmp
 	if ! tmp=$(complete -p "$cmd" 2>/dev/null); then
-		# echo _complete_alias::do::alias sourcing /usr/share/bash-completion/completions/$cmd
+		# echo _aLiAs_CoMpLeTe_::do::alias sourcing /usr/share/bash-completion/completions/$cmd
+		# shellcheck disable=SC1090
 		if ! . "/usr/share/bash-completion/completions/$cmd"; then
-			echo "complete_alias: Sourcing /usr/share/bash-completion/completions/$cmd failed" >&2
+			echo "alias_complete: Sourcing /usr/share/bash-completion/completions/$cmd failed" >&2
 			return 2
 		fi
 		if ! tmp=$(complete -p "$cmd" 2>/dev/null); then
-			echo "complete_alias: No completion found for $cmd" >&2
+			echo "alias_complete: No completion found for $cmd" >&2
 			return 2
 		fi
 	fi
 
-	# echo "_complete_alias::do::alias complete -p $cmd -> $tmp"
-	local complete_func complete_args cmd_with_args cmd_args_cnt cmd_with_args_escaped 
-	local complete_args_escaped new_complete_func
-	tmp=$(<<<"$tmp" tr ' ' '\n' | sed '$d')
-	complete_func=$(<<<"$tmp" grep -A1 -- '-F' | tail -n1)
-	if ! declare -f "$complete_func" >/dev/null; then
-		echo "complete_alias: internal error: $complete_func does not exists"  >&2
+	# echo "_aLiAs_CoMpLeTe_::do::alias complete -p $cmd -> $tmp"
+	local complete_cmd IFS
+	IFS=' '
+	read -r -a complete_cmd <<<"$tmp"
+
+	if [[ "${complete_cmd[-1]}" != "$cmd" ]]; then
+		echo "alias_complete: Internal error: Something very wrong happened" >&2
+		return 2
+	fi
+	unset 'complete_cmd[-1]'
+
+	local i next_is_func complete_args complete_func
+	next_is_func=false
+	for i in "${complete_cmd[@]}"; do
+		if [[ "$i" == "-F" ]]; then
+			next_is_func=true;
+			continue
+		fi
+		if "$next_is_func"; then
+			complete_func="$i"
+			continue
+		fi
+		complete_args+=("$i")
+	done
+
+	if [[ -z "${complete_func:-}" ]]; then
+		echo "alias_complete: error: for it to work, aliased command has to be completed with a function" >&2
 		return 1
 	fi
-	complete_args=($(<<<"$tmp" grep -v -x -- '-F\|'"${complete_func}"))
+	if ! declare -f "$complete_func" >/dev/null; then
+		echo "alias_complete: internal error: $complete_func does not exists"  >&2
+		return 2
+	fi
 
-	cmd_with_args=("$cmd" "${cmd_args[@]}")
-	cmd_args_cnt=${#cmd_args[@]}
-	cmd_with_args_escaped=$(printf " %q" "$cmd" "${cmd_args[@]}" | cut -c2-)
+	local cmd_with_args_star_len
+	cmd_with_args_star_len="$(IFS=" "; echo "${cmd_with_args[*]}")"
+	cmd_with_args_star_len="${#cmd_with_args_star_len}"
 
-	eval "$(cat <<EOF2
-	_complete_alias::$alias_name() {
-		# echo "$new_complete_func: '\$#' '\$1' '\$2' '\$3' '\$COMP_CWORD' '\${COMP_WORDS[@]}' '\$COMP_LINE' '\$COMP_POINT'"
+	eval '_aLiAs_CoMpLeTe_::'"$alias_name"'() {
+		# echo >&2
+		# echo "_aLiAs_CoMpLeTe_ \$#=\"$#\" \$1=\"$1\" \$2=\"$2\" \$3=\"$3\" CWORD=\"$COMP_CWORD\"" >&2
+		# echo "_aLiAs_CoMpLeTe_ WORDS=\"$(IFS="|"; echo "${COMP_WORDS[*]}")\" LINE=\"$COMP_LINE\" POINT=\"$COMP_POINT\"" >&2
+
+		local          cmd_with_args alias_name complete_func cmd_with_args_star_len
+		'"$(declare -p cmd_with_args alias_name complete_func cmd_with_args_star_len)"'
+
 		if ((COMP_CWORD == 1)); then
-			set -- "$cmd" "\$2" "${cmd_with_args[-1]}"
+			set -- "${cmd_with_args[0]}" "$2" "${cmd_with_args[-1]}"
 		else
-			set -- "$cmd" "\$2" "\$3"
+			set -- "${cmd_with_args[0]}" "$2" "$3"
 		fi
-		((COMP_CWORD += $cmd_args_cnt)) ||:
-		COMP_WORDS=($cmd_with_args_escaped \${COMP_WORDS[@]:1})
-		COMP_LINE="$cmd_with_args_escaped\${COMP_LINE#"$alias_name"}"
-		((COMP_POINT += ${#cmd_with_args_escaped} - ${#alias_name})) ||:
-		# echo "$new_complete_func: '\$#' '\$1' '\$2' '\$3' '\$COMP_CWORD' '\${COMP_WORDS[@]}' '\$COMP_LINE' '\$COMP_POINT'"
-	
-		"$complete_func" "\$@"
+		((COMP_CWORD += ${#cmd_with_args[@]} - 1)) ||:
+		COMP_WORDS=("${cmd_with_args[@]}" "${COMP_WORDS[@]:1}")
+		COMP_LINE="$(IFS=" "; echo "${cmd_with_args[*]}")${COMP_LINE#"$alias_name"}"
+		((COMP_POINT += ${cmd_with_args_star_len} - ${#alias_name})) ||:
+		
+		# echo "_aLiAs_CoMpLeTe_ \$#=\"$#\" \$1=\"$1\" \$2=\"$2\" \$3=\"$3\" CWORD=\"$COMP_CWORD\"" >&2
+		# echo "_aLiAs_CoMpLeTe_ WORDS=\"$(IFS="|"; echo "${COMP_WORDS[*]}")\" LINE=\"$COMP_LINE\" POINT=\"$COMP_POINT\"" >&2
+
+		"$complete_func" "$@"
+	}'
+	"${complete_args[@]}" -F "_aLiAs_CoMpLeTe_::$alias_name" "$alias_name"
+}
+
+# Main #########################
+
+if "$_aLiAs_CoMpLeTe__longopt"; then
+	if (($# > 1)); then
+		_aLiAs_CoMpLeTe__fatal "The --longopt options uses only alias name."
+	fi
+	complete -F "_aLiAs_CoMpLeTe_::longopt" "$1"
+elif "$_aLiAs_CoMpLeTe__shortopt"; then
+	if (($# > 1)); then
+		_aLiAs_CoMpLeTe__fatal "The --shortopt options uses only alias name"
+	fi
+	complete -F "_aLiAs_CoMpLeTe_::shortopt" "$1"
+else
+	_aLiAs_CoMpLeTe__alias_name="$1"
+	if (($# < 2)); then
+		_aLiAs_CoMpLeTe__fatal "Command argument exprected"
+	fi
+	if [[ -z "$2" ]]; then
+		_aLiAs_CoMpLeTe__fatal "Command is empty"
+	fi
+	_aLiAs_CoMpLeTe__cmd="$2"
+	shift 2
+	_aLiAs_CoMpLeTe__args=("$@")
+
+	# Do the normal completion
+
+	if "$_aLiAs_CoMpLeTe__withalias"; then
+		# shellcheck disable=SC2139
+		alias "$_aLiAs_CoMpLeTe__alias_name=$(printf "%q " "$cmd" "$@")"
+	fi
+
+	eval '
+	_aLiAs_CoMpLeTe_::setup::'"$_aLiAs_CoMpLeTe__alias_name"'() {
+		local _aLiAs_CoMpLeTe__alias_name _aLiAs_CoMpLeTe__cmd _aLiAs_CoMpLeTe__args
+		'"$(declare -p _aLiAs_CoMpLeTe__alias_name _aLiAs_CoMpLeTe__cmd _aLiAs_CoMpLeTe__args)"'
+		_aLiAs_CoMpLeTe_::do::alias "$_aLiAs_CoMpLeTe__alias_name" "$_aLiAs_CoMpLeTe__cmd" "${_aLiAs_CoMpLeTe__args[@]}" &&
+		_aLiAs_CoMpLeTe_::"$_aLiAs_CoMpLeTe__alias_name" "$@"
 	}
-EOF2
-	)"
-	"${complete_args[@]}" -F "_complete_alias::$alias_name" "$alias_name"
-}
-
-cat <<EOF
-$(declare -f _complete_alias::do::alias)
-_complete_alias::setup::$alias_name() {
-	local alias_name cmd cmd_args
-	alias_name=$(printf "%q" "$alias_name")
-	cmd=$(printf "%q" "$cmd")
-	cmd_args=($( ((${#cmd_args[@]})) && printf "%q " "${cmd_args[@]}" || :))
-EOF
-cat <<'EOF'
-	_complete_alias::do::alias "$alias_name" "$cmd" "${cmd_args[@]}" &&
-	_complete_alias::$alias_name "$@"
-}
-EOF
-cat <<EOF
-complete -F '_complete_alias::setup::$alias_name' '$alias_name'
-EOF
-
-)
-
-if ! bash >/dev/null <<EOF
-$output
-complete -p '$alias_name' || exit 1
-declare -F _complete_alias::do::alias '_complete_alias::setup::$alias_name' || exit 1
-EOF
-then
-	fatal "Internal error: Output from this script is invalid."
+	'
+	complete -F "_aLiAs_CoMpLeTe_::setup::$_aLiAs_CoMpLeTe__alias_name" "$_aLiAs_CoMpLeTe__alias_name"
 fi
 
-cat <<<"$output"
+_aLiAs_CoMpLeTe__clearenv
 
--- etc/bash.d/arch.bashrc
+++ etc/bash.d/arch.bashrc
@@ -24,9 +24,9 @@ _archlinux_pacman() {
 }
 
 p() { _archlinux_pacman "$@"; }
-eval "$(alias_complete.sh p pacman)"
+. alias_complete.sh p pacman
 pn() { p --noconfirm "$@"; }
-eval "$(alias_complete.sh pn pacman)"
+. alias_complete.sh pn pacman
 pupdate() { 
 	local tmp &&
 	tmp=$(pacman -Q | cut -d' ' -f1 | grep '[^ ]*-keyring') &&
@@ -39,9 +39,41 @@ pupdate() {
 		p --noconfirm -R $tmp
 	fi
 }
-eval "$(alias_complete.sh pupdate pacman)"
+. alias_complete.sh pupdate pacman
 pacmann() { pacman --noconfirm "$@"; }
-eval "$(alias_complete.sh pacmann pacman)"
+. alias_complete.sh pacmann pacman
 yayn() { yay --noconfirm "$@"; }
-eval "$(alias_complete.sh yayn yay)"
+. alias_complete.sh yayn yay
+
+,pacman_list_packages_by_size() {
+	pacman -Qii | 
+	awk -F: '/^Name *:/{n=$2} /^Installed Size *:/{print n, $2}' |
+	awk '{print $2 gensub("B$", "", "g", $3), $1, $2 $3 }' |
+	numfmt --field 1 --from=auto |
+	sort -k1n
+}
+
+,paccheck() {
+	local name failed mtree md5sum_files md5sum_dirs failed
+	name=$1
+	failed=0
+	mtree=$(printf "%s\n" /var/lib/pacman/local/${name}-[0-9]*/mtree | head -n 1)
+	mtree=$(zcat "$mtree")
+	if ! md5sum_files=$(echo "$mtree" | grep -v "\./\." | sed -n '/md5digest/s/\.\?\([^\ ]*\) .* md5digest=\([^\ ]*\) .*/\2 \1/p' | md5sum -c -); then
+		echo "md5sum failed on files:"
+		echo "$md5sum_files" | grep -v ": OK$"
+		(( failed++ ))
+	fi
+	
+	if ! md5sum_dirs=$(echo "$mtree" | grep -v "\./\." | grep type=dir | sed 's/^\.//' | while read path _; do if [ -d "$path" ]; then echo "$path: OK"; else echo "$path: FAILED"; fi; done;); then
+		echo "md5sum failed on dirs"
+		echo "$md5sum_dirs" | grep -v ": OK$"
+		(( failed++ ))
+	fi
+	
+	if (( failed == 0 )); then
+		echo "OK"
+	fi
+	return "$failed"
+}
 
