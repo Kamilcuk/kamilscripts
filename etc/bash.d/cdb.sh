9deb6f452a4c9effbafbc988db7b1143e808998d
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-09-27 21:46:52 +0200

    cdb: Remove CDB_DIR environment variable from beeing accessible
    Handle it locally within functions

--- etc/bash.d/cdb.sh
+++ etc/bash.d/cdb.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # usage below
+
 #if hash cdb 2>/dev/null >/dev/null; then
 #	return
 #fi
 
-export CDB_DIR="$HOME/.cache/cdb"
-
 cdb() {
+	local cdb_dir=${CDB_DIR:-"$HOME/.cache/cdb"}
 	case "${1:-}" in
 	-u|--unmark|-g|--get)
 		if (($# != 2)); then "cdb: Wrong number of arguments: $#" >&2; return 2; fi
 		local names i
 		names=()
-		if [[ "$2" != '.' && "$2" != '..' && -L "$CDB_DIR/$2" ]]; then
-			names+=("$CDB_DIR/$2")
+		if [[ "$2" != '.' && "$2" != '..' && -L "$cdb_dir/$2" ]]; then
+			names+=("$cdb_dir/$2")
 		elif [[ -d "$2" ]]; then
-			for i in "$CDB_DIR"/*; do
+			for i in "$cdb_dir"/*; do
 				if [[ ! -L "$i" ]]; then continue; fi
 				if [[ "$i" -ef "$2" ]]; then
 					names+=("$i")
@@ -25,7 +25,7 @@ cdb() {
 		if ((!${#names})); then echo "cdb: Mark does not exists: $2" >&2; return 1; fi
 		for i in "${names[@]}"; do
 			if [[ ! -L "$i" ]]; then echo "cdb: Internal error 1" >&2; return 2; fi
-			if [[ ! "$i" -ef "$CDB_DIR/$(basename "$i")" ]]; then echo "cdb: Internal error 1" >&2; return 2; fi
+			if [[ ! "$i" -ef "$cdb_dir/$(basename "$i")" ]]; then echo "cdb: Internal error 1" >&2; return 2; fi
 		done
 		;;
 	esac
@@ -47,11 +47,11 @@ cdb() {
 		fi
 		if [[ -z "$name" ]]; then echo "cdb: Error: name is empty: $name" >&2; return 2; fi
 		if [[ "$name" = "." || "$name" = ".." ]]; then echo "cdb: Error: invalid name: $name" >&2; return 2; fi
-		if [[ -e "$CDB_DIR/$name" ]]; then echo "cdb: Mark already exists: $name -> $(readlink -f "$CDB_DIR/$name")" >&2; return 1; fi
+		if [[ -e "$cdb_dir/$name" ]]; then echo "cdb: Mark already exists: $name -> $(readlink -f "$cdb_dir/$name")" >&2; return 1; fi
 		if ! (
 				dest=$(cd "$dest" && echo "$PWD") &&
-				mkdir -p "$CDB_DIR" &&
-				cd "$CDB_DIR" &&
+				mkdir -p "$cdb_dir" &&
+				cd "$cdb_dir" &&
 				ln -v -s "$dest" "$name"
 		); then
 			echo "cdb: Failed to create the link" >&2
@@ -67,14 +67,14 @@ cdb() {
 		;;
 	-g|--get)
 		# names is set above
-		pushd "$CDB_DIR" >/dev/null 
+		pushd "$cdb_dir" >/dev/null 
 		find "${names[@]}" -type l -printf '%f -> %l\n' | sort
 		popd >/dev/null
 		;;
 	-l|--list)
 		if (($# != 1)); then echo "cdb: Wrong number of arguments" >&2; return 2; fi
-		if [[ ! -e "$CDB_DIR" ]]; then return; fi
-		find "$CDB_DIR" -type l -printf '%f -> %l\n' | sort
+		if [[ ! -e "$cdb_dir" ]]; then return; fi
+		find "$cdb_dir" -type l -printf '%f -> %l\n' | sort
 		;;
 	*)
 		if (($# != 1)) || [[ "$1" = -h ]] || [[ "$1" == "--help" ]]; then 
@@ -120,21 +120,22 @@ Options:
   		Navigate to bookmark named <name>.
 
 Bookmarks are created in the directory referenced by
-environment variable CDB_DIR. Currently:
-    CDB_DIR=$CDB_DIR
+environment variable cdb_dir. Currently:
+    cdb_dir=$cdb_dir
 
 Written by Kamil Cukrowski
 EOF
 			return
 		fi
 		if (($# != 1)); then echo "cdb: Wrong number of arguments" >&2; return 2; fi
-		if [[ ! -e "$CDB_DIR/$1" ]]; then echo "cdb: No such mark: $1" 2>&1; return 1; fi
-		if ! cd -P "$CDB_DIR/$1"; then echo "cdb: Error changing directory: $1" 2>&1; return 2; fi
+		if [[ ! -e "$cdb_dir/$1" ]]; then echo "cdb: No such mark: $1" 2>&1; return 1; fi
+		if ! cd -P "$cdb_dir/$1"; then echo "cdb: Error changing directory: $1" 2>&1; return 2; fi
 		;;
 	esac
 }
 
 _cdb_completion() {
+	local cdb_dir=${CDB_DIR:-"$HOME/.cache/cdb"}
 	# if ((COMP_CWORD > 2)); then return; fi
 	if ((COMP_CWORD == 1)); then
   		case "${COMP_WORDS[COMP_CWORD]}" in
@@ -142,8 +143,8 @@ _cdb_completion() {
 			COMPREPLY=($(compgen -W '-m --mark -u --unmark -l --list -h --help' -- "${COMP_WORDS[COMP_CWORD]}"))
 			;;
 		*)
-			if [[ ! -e "$CDB_DIR" ]]; then return; fi
-			pushd "$CDB_DIR" >/dev/null
+			if [[ ! -e "$cdb_dir" ]]; then return; fi
+			pushd "$cdb_dir" >/dev/null
 			COMPREPLY=($(compgen -o dirnames -d -- "${COMP_WORDS[COMP_CWORD]}"))
 			popd >/dev/null
 			if ((!${#COMPREPLY})); then
@@ -157,8 +158,8 @@ _cdb_completion() {
 			COMPREPLY=($(compgen -o dirnames -d -- "${COMP_WORDS[COMP_CWORD]}"))
 			;;
 		-u|--unmark|-g|--get)
-			if [[ ! -e "$CDB_DIR" ]]; then return; fi
-			pushd "$CDB_DIR" >/dev/null
+			if [[ ! -e "$cdb_dir" ]]; then return; fi
+			pushd "$cdb_dir" >/dev/null
 			COMPREPLY=($(compgen -o dirnames -- "${COMP_WORDS[COMP_CWORD]}"))
 			popd >/dev/null
 			;;
