1ee5999d0776c71efc0a53df31098bf959c5b651
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-10-22 00:19:09 +0200

    Add modinfo_parameters script

---
+++ bin/,modinfo_parameters.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+name=$(basename "$0")
+
+usage() {
+	cat <<EOF
+Usage: $name modulename
+
+Print module parameters with values if loaded.
+
+EOF
+}
+if ((!$#)); then
+	usage
+	exit
+fi
+
+m=$1
+d=/sys/module/$m/parameters/
+if [ ! -d "$d" ]; then
+	fatal "Module $m not loaded"
+fi
+
+modinfo "$m" | sed -n 's/^parm: *//p' |
+sort |
+awk -F: -v "d=$d" -v OFS=':' '{ f = d $1; getline v < f; print $0, v}'
+
