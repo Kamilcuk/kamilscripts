8c27b19ad0d6e29320f74ea681906a2fa48c603e
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-10-22 00:18:26 +0200

    Do not print calendar on su

--- etc/bash.d/zz_entry.sh
+++ etc/bash.d/zz_entry.sh
@@ -1,5 +1,9 @@
 [[ $- != *i* ]] && return
 
+# https://stackoverflow.com/questions/4261876/check-if-bash-script-was-invoked-from-a-shell-or-another-script-application
+# do not show calendar when su-ing
+[[ ! $(ps -o stat= -p $$) =~ s && "$SHLVL" == 1 ]] && return
+
 # Put your local functions and aliases herea
 if hash cal 2>/dev/null && [[ $(cal --version 2>&1) =~ "util-linux" ]]; then
 	hash color.sh 2>/dev/null && color.sh green
