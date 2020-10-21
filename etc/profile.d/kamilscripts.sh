e0ac345185082fc3a5bcb42986db56f3930b7060
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-10-22 00:18:53 +0200

    kamilscripts - add HISTSIZE, update indentation

--- etc/profile.d/kamilscripts.sh
+++ etc/profile.d/kamilscripts.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 appendpath () {
-    case ":$PATH:" in
-        "$1":*|*:"$1":*|*:"$1") ;;
-        *) PATH="${PATH:+$PATH:}$1"; ;;
-    esac
+	case ":$PATH:" in
+		"$1":*|*:"$1":*|*:"$1") ;;
+		*) PATH="${PATH:+$PATH:}$1"; ;;
+	esac
 }
 
 appendpath /usr/local/sbin
@@ -16,23 +16,26 @@ appendpath /bin
 if [ -d "$HOME" ]; then
 	appendpath "$HOME"/bin
 	appendpath "$HOME"/.config/bin
-	if [ -e "$HOME"/.config/kamilscripts/kamilscripts ]; then
-		appendpath "$HOME"/.config/kamilscripts/kamilscripts/bin
-		KCDIR="$HOME"/.config/kamilscripts/kamilscripts
-	fi
 fi
-if [ -d /usr/lib/kamilscripts/bin ]; then
+if [ -d "$HOME" ] && [ -e "$HOME"/.config/kamilscripts/kamilscripts ]; then
+	appendpath "$HOME"/.config/kamilscripts/kamilscripts/bin
+	KCDIR="$HOME"/.config/kamilscripts/kamilscripts
+elif [ -d /usr/lib/kamilscripts/bin ]; then
 	appendpath /usr/lib/kamilscripts/bin
 	KCDIR=/usr/lib/kamilscripts
 fi
 unset appendpath
 export PATH
 
-# set some environmental variables
-export EDITOR="$(command -v vim)"
-export VISUAL="$EDITOR"
+# https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html
+if command -v vim >/dev/null 2>&1; then
+	export EDITOR=vim
+	export VISUAL=vim
+fi
 export TMPDIR=/tmp
 export COUNTRY=PL
+export HISTSIZE=
+export HISTFILESIZE=
 mesg y
 
 # https://wiki.archlinux.org/index.php/Makepkg
