4b4441a903b027aeacf2382d2797432309f2b1d3
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-12-07 01:40:06 +0100

    bash: cosmetics and try to speed up zz_entry

--- etc/bash.d/zz_entry.sh
+++ etc/bash.d/zz_entry.sh
@@ -1,15 +1,15 @@
-[[ $- != *i* ]] && return
+if [[ $- != *i* ]]; then return; fi
 
 # https://stackoverflow.com/questions/4261876/check-if-bash-script-was-invoked-from-a-shell-or-another-script-application
 # do not show calendar when su-ing
-[[ ! $(ps -o stat= -p $$) =~ s && "$SHLVL" == 1 ]] && return
+if ! [[ $(ps -o stat= -p $$) =~ s && -t 0 ]]
+then return; fi
 
 # Put your local functions and aliases herea
-if hash cal 2>/dev/null && [[ $(cal --version 2>&1) =~ "util-linux" ]]; then
-	hash color.sh 2>/dev/null && color.sh green
+if hash cal 2>/dev/null; then
 	if hash ,color 2>/dev/null; then ,color green; fi
 	cal -3m
 	if hash ,color 2>/dev/null; then ,color reset; fi
 fi
-hash fortune 2>/dev/null && fortune
+if hash fortune 2>/dev/null; then fortune; fi
 
