b51a75bad2198d0dfcabc14823fca84bfacc047a
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-12-14 01:03:44 +0100

    kamilscripts_stow: Completely move to GNU stow with installation script

--- bin/,kamilscripts_stow.sh
+++ bin/,kamilscripts_stow.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-case "$BASH_VERSION" in 4.*) ;; *)
-set -euo pipefail ;;
-esac
+set -euo pipefail
 
 name=$(basename "$0")
-dir="$(readlink -f "$(dirname "$(readlink -f "$0")")"/../stow)"
+dir=$(readlink -f "$(dirname "$(readlink -f "$0")")")
 
 usage() {
 	cat <<EOF
@@ -21,51 +19,118 @@ Modes:
 EOF
 }
 
+git_submodule_update() {
+	if ! "$dryrun"; then
+		(
+		set -xeuo pipefail
+		cd "$dir"
+		local tmp
+		tmp="$(git rev-parse --show-toplevel)"
+		cd "$tmp"
+		git submodule update --recursive --init
+		)
+	fi
+}
+
+stow_manage_installation() {
+	local d b
+	d=~/.config/kamilscripts/stow
+	b=~/.config/bin
+	if hash stow 2>/dev/null >&2; then
+		if [[ -e "$b"/stow && -d "$d" && ( -x /usr/bin/stow || -x /bin/stow ) ]]; then
+			echo "### System stow installation exists, removing our installation"
+			( set -x
+			rm -rf "$d" "$b"/stow "$b"/chkconfig
+			)
+		fi
+		if ! hash stow 2>/dev/null >&2; then
+			fatal "Something went horribly wrong when removing stow"
+		fi
+		return
+	fi
+	if [[ ! -x "$d/stow" ]] || ! "$d"/stow --version 2>/dev/null >&2; then
+		echo "### Installing stow..." >&2
+		( set -xeuo pipefail
+		rm -rf "$d" &&
+		mkdir -p "$d"/src &&
+		cd "$d"/src &&
+		wget https://ftp.gnu.org/gnu/stow/stow-latest.tar.gz &&
+		tar xaf stow-latest.tar.gz --strip-components=1 -C . &&
+		./configure --quiet --prefix="$d" --with-pmdir="$d" --bindir="$d" &&
+		make install MAKEINFO=true &&
+		cd "$d" &&
+		rm -rf "$d"/src/ "$d"/share/
+		)
+		if ! "$d"/stow --version 2>/dev/null >&2; then
+			echo "### stow installed unsuccessfully"
+			exit 1
+		fi
+		mkdir -p "$b"
+		ln -fs "$d"/stow "$b"/stow
+		ln -fs "$d"/chkstow "$b"/chkstow
+		echo "### stow installed!" >&2
+	fi
+	export PATH="$PATH:$b"
+}
+
+do_stow() {
+	local repo
+	repo=$1
+	shift
+
+	if "$dryrun"; then
+		set -- '-n' "$@"
+	fi
+	set -- '-v' "$@"
+
+	if [[ ! -r "$repo"/.stowrc ]]; then
+		fatal "$repo/.stowrc not such file"
+	fi
+
+	echo "+" "cd" "$repo" "&&" stow "$@"
+	pushd "$repo" >/dev/null
+	command stow "$@"
+	popd >/dev/null
+}
+
+stow_kamilscripts() {
+	local packages
+	packages=(common "$HOSTNAME")
+	case "$HOSTNAME" in
+	gucio|bocian.cis.gov.pl) packages+=(fix_term_missing_italic); ;;
+	esac
+
+	do_stow "$(readlink -f "$dir/../stow")" "$@" "${packages[@]}"
+}
+
+stow_work() {
+	stow_manage_installation
+
+	if "$dryrun"; then
+		echo "Dry run. Add --ok to actually run"
+	fi
+
+	stow_kamilscripts "$@"
+}
+
+# main ##############################################################################
+
 args=$(getopt -n "$name" -o hk -l help,ok -- "$@")
 eval set -- "$args"
-ok=false
+dryrun=true
 while (($#)); do
 	case "$1" in
 	-h|--help) usage; exit 0; ;;
-	-k|--ok) ok=true; ;;
+	-k|--ok) dryrun=false; ;;
 	--) shift; break; ;;
 	esac
 	shift
 done
 
-stowargs=()
-if ! "$ok"; then
-	echo "Dry run. Add --ok to actually run"
-	stowargs+=(-n)
-fi
-
-export PATH="$PATH:$(dirname "$BASH_SOURCE")/../bin"
-if ! hash qstow 2>/dev/null; then
-	echo "ERROR: qstow not found" >&2
-fi
-
-run() {
-	echo "+" "$@"
-	"$@"
-}
-
-s() {
-	local flags
-	flags=("${stowargs[@]}" "$@" common)
-	if [[ -d "$dir/$HOSTNAME" ]]; then flags+=("$HOSTNAME"); fi
-	for i in /usr/lib/kamilscripts /lib/kamilscripts; do
-		if [[ ! -e "$i" ]]; then
-			flags+=("noinstall")
-			break
-		fi
-	done
-	run qstow -v -t "$HOME" -d "$dir" "${flags[@]}"
-}
-
 if (($#==0)); then usage; exit 1; fi
 case "$1" in
-i*)    s -R; if "$ok"; then ( set -x && cd "$dir" && cd "$(git rev-parse --show-toplevel)" && git submodule update --recursive --init; ); fi; ;;
-u*)    s -D ;;
+i|install)    stow_work -R; ;;
+u|uninstall)  stow_work -D; ;;
 *) echo "Unknown mode: $1" >&2; ;;
 esac
 
