baef92eba409d237a168ada9e4eb94fe55ba90cd
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-12-14 11:46:58 +0100

    Fix handling on systems with missing tic command from ncurses

--- stow/fix_term_missing_italic/.config/bash.d/00_fix_term_missing_italic.sh
+++ stow/fix_term_missing_italic/.config/bash.d/00_fix_term_missing_italic.sh
@@ -1,14 +1,26 @@
 
 # https://alexpearce.me/2014/05/italics-in-iterm2-vim-tmux/
+if ! hash tput 2>/dev/null >&2; then return; fi; # no ncurses, no point
 if [[ "$({ tput sitm; } 2>/dev/null)" != "" ]]; then return; fi
 
+newterm=""
 case "$TERM" in
-	screen-256color)
-		tic "${BASH_SOURCE%/*}"/screen-256color-italic.terminfo
-		export TERM=screen-256color-italic
-		;;
-	xterm-256color)
-		tic "${BASH_SOURCE%/*}"/xterm-256color-italic.terminfo
-		export TERM=xterm-256color-italic
-		;;
+screen-256color)
+	newterm=screen-256color-italic
+	;;
+xterm-256color)
+	newterm=xterm-256color-italic
+	;;
 esac
+
+if [[ -n "$newterm" ]]; then
+	if 
+		[[ -e "$HOME/.terminfo/${newterm:0:1}/$newterm" ]] || 
+		{ hash toe 2>/dev/null >&2 && { toe -a | grep -q "^$newterm\s\+" ;} ;} ||
+		{ hash tic 2>/dev/null >&2 && tic "$(dirname "$BASH_SOURCE")"/"$newterm".terminfo ;}
+	then
+		export TERM="$newterm"
+	fi
+fi
+unset newterm
+
