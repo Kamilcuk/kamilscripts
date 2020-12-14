21cfe46b7e319af687ba8a222495115aa19676d4
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-12-14 11:11:39 +0100

    Add gitmodules links change script

---
+++ gitmodules_links_change.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -euo pipefail
+
+to_ssh() {
+	sed -E 's~^(\s*url\s*=\s*)https://github.com/kamilcuk/~\1git@github.com:kamilcuk/~' -i .gitmodules
+}
+to_https() {
+	sed -E 's~^(\s*url\s*=\s*)git@github.com:kamilcuk/~\1https://github.com/kamilcuk/~' -i .gitmodules
+}
+detect() {
+	sed -E '
+		\~^\s*url\s*=\s*https://github.com/kamilcuk~{ s/.*/https/; q }
+		\~^\s*url\s*=\s*git@github.com:kamilcuk/~{ s/.*/ssh/; q }
+		d
+	' .gitmodules
+}
+syncme() {
+	diff --old-line-format=$'- %l\n' --new-line-format=$'+ %l\n' --old-group-format='%<' --new-group-format='%>' --changed-group-format='%<%>' --unchanged-group-format='' <(cat <<<"$tmp") .gitmodules ||:
+	git submodule sync
+}
+usage() {
+	cat <<EOF
+Usage: $(basename "$0") [https|ssh|detect|auto]
+EOF
+	exit 1
+}
+
+if (($# > 1)); then usage; fi;
+
+tmp=$(cat .gitmodules)
+case "${1:-auto}" in
+http|https) to_https; syncme; ;;
+git|ssh) to_ssh; syncme; ;;
+detect) detect; ;;
+auto)
+	a="$(detect)"; 
+	case "$a" in
+	https) a=ssh; ;;
+	ssh) a=https; ;;
+	esac
+	echo "to $a"
+	"$0" "$a"
+	;;
+*) usage; ;;
+esac
+
+
