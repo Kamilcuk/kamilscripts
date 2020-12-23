2d7316e6ea0c6fb92ef7be5b9b9caaf2b063846c
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-12-23 11:36:02 +0100

    try_autorun: Detect sanitize options

--- bin/,try_autorun.sh
+++ bin/,try_autorun.sh
@@ -27,14 +27,17 @@ be_nice() {
 
 usage() {
 	cat <<EOF
-Usage: $name [options] <file>
+Usage: $name [options] <file> [tool_specific_arguments...]
 
 Tries to compile and automatically run the file or the project.
+Detects filetype using the extension of the file.
+Chooses from gcc, g++, python, bash.
+In project mode chooses from: make, cmake.
 
 Options:
    -e filetype   Pass filetype as vim filetype.
    -S            Synchronize stderr with stdout and be line buffered.
-   -p            Try to detect project files: makefile, cmake, etc..
+   -p            Try to detect project files before detecting filetype.
    -n            Dry run.
    -h            Print this help and exit.
 
@@ -61,34 +64,87 @@ parse_arguments() {
 		*) fatal "Invalid argument: -$OPTARG"; ;;
 		esac
 	done
-	shift "$((OPTIND-1))"
-	if (($# != 1)); then
-		fatal "Invalid count of arguments"
-	fi		
-	file=$1
 }
-	
+
+gcc_version() {
+	if ! gcc --version | grep '^gcc' | sed 's/^.* \([0-9]\+\.[0-9]\+\).*/\1/'; then
+		fatal "Getting gcc version failed"
+	fi
+}
+
+gcc_try_gcc_compile() {
+	tmpf=$(mktemp --tmpdir tmp.try_autorun.XXXXXXXXXX.c)
+	trap 'rm "$tmpf"' EXIT
+	echo 'int main() { return 0; }' > "$tmpf"
+	LC_ALL=C gcc "${cflags[@]}" "${sanitize[@]}" "$tmpf" </dev/null
+}
+
+awkeval() {
+	if (($# != 1)); then fatal "internal error: awkeval takes one argument"; fi
+	awk 'BEGIN{exit(!('"$1"'))}' <&-
+}
+
+awkeval_verbose() {
+	if awkeval "$1"; then echo "$1 is true"; else echo "$1 is false"; return 1; fi
+}
+
+cflags_detect() {
+	local base
+	base=(
+		-Wall
+		-Wextra
+		-ggdb3
+	)
+	# global cflags
+	cflags=("${base[@]}")
+
+	local ver
+	if ! ver=$(gcc_version); then
+		fatal "Could not get gcc version"
+	fi
+
+	local sanitize
+	if awkeval "$ver >= 4.8"; then
+		sanitize+=(-fsanitize=address)
+	fi
+	if awkeval "$ver >= 5"; then
+		sanitize+=(-fsanitize=undefined)
+	fi
+	if awkeval "$ver >= 8"; then
+		sanitize+=(-fsanitize=pointer-compare -fsanitize=pointer-subtract)
+	fi
+	local tmp
+	if ((${#sanitize[@]} != 0)) && tmp=$(gcc_try_gcc_compile "${cflags[@]}" "${sanitize[@]}" 2>&1); then
+		cflags+=("${sanitize[@]}")
+	fi
+}
+
 # main #########################################
 
 be_nice "$@"
 
 parse_arguments "$@"
-
-cflags=(
-	-Wall -Wextra
-	-ggdb3
-	-fsanitize=address -fsanitize=undefined -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize-address-use-after-scope
-)
+shift "$((OPTIND-1))"
+if (($# != 1)); then usage; fatal "Invalid count of arguments"; fi
+file=$1
+shift
 
 if "$use_project"; then
 	if [[ -e 'Makefile' ]]; then
-		run make
+		cflags_detect
+		export CFLAGS="${cflags[*]}"
+		export CXXFLAGS="${cflags[*]}"
+		run make "$@"
 		exit
 	elif [[ -e 'CMakeLists.txt' ]] &&
 			grep -q 'cmake_minimum_required\s*\(\s*VERSION.*\)' 'CMakeLists.txt'; then
 		args=()
 		if hash ninja 2>/dev/null; then args+=(-G Ninja); fi
+		cflags_detect
 
+		if [[ -e "_build" && ! -e "_build/CMakeCache.txt" ]]; then
+			fatal "cmake _build directory exists and is not cmake build dir. Bailing out"
+		fi
 		run cmake -S. -B_build "${args[@]}" \
 			-DCMAKE_VERBOSE_MAKEFILE=yes \
 			-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
@@ -97,6 +153,7 @@ if "$use_project"; then
 			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY="$PWD"/_build/bin \
 			-DCMAKE_LIBRARY_OUTPUT_DIRECTORY="$PWD"/_build/lib \
 			-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="$PWD"/_build/lib \
+			"$@" \
 			&&
 		run cmake --build _build &&
 		( cd _build && run ctest -V )
@@ -115,6 +172,7 @@ cpp|cxx|cc|c++)
 	if [[ -z "$ccompiler" ]] || ! hash "$ccompiler" 2>/dev/null; then
 		fatal "Compiler could not be found: $ccompiler"
 	fi
+	cflags_detect
 	tmp=$(mktemp --suffix='.out')
 	trap 'rm "$tmp"' EXIT
 	{
@@ -122,16 +180,16 @@ cpp|cxx|cc|c++)
 			exec 1>&2
 		fi
 		echo "+" "$ccompiler" "${cflags[@]}" "$file" >&2
-		"$ccompiler" "${cflags[@]}" -o "$tmp" "$file"
+		"$ccompiler" "${cflags[@]}" "$@" -o "$tmp" "$file"
 		"$tmp"
 	}
 	# tmp removed on EXIT
 	;;
 py)
-	run python "$file"
+	run python "$@" "$file"
 	;;
 sh|bash|bashrc)
-	run bash "$file"
+	run bash "$@" "$file"
 	;;
 *) 
 	fatal "Don't know how to build $file"
