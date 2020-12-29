257fe77d456bf9175a78f49417fe2dd80296040e
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-12-29 12:23:35 +0100

    Add locale configurstion to proffile

--- etc/profile.d/defaults.sh
+++ etc/profile.d/defaults.sh
@@ -41,3 +41,34 @@ export npm_config_prefix="$HOME/.node_modules"
 
 unset -f appendpath
 
+# locale ##############################################################################
+
+locale_supported() {
+	[ -z "$( { LC_ALL=$1 ;} 2>&1 )" ]
+}
+en_US=""
+if locale_supported en_US.UTF-8; then
+	en_US="en_US.UTF-8"
+fi
+pl_PL=""
+if locale_supported pl_PL.UTF-8; then
+	pl_PL="pl_PL.UTF-8"
+fi
+unset -f locale_supported
+
+export LANG=${en_US:-C}
+export LC_CTYPE=${pl_PL:-${en_US:-C}}
+export LC_NUMERIC=${en_US:-C}
+export LC_TIME=${pl_PL:-${en_US:-C}}
+export LC_COLLATE=C
+export LC_MONETARY=${en_US:-C}
+export LC_MESSAGES=${en_US:-C}
+export LC_PAPER=${en_US:-C}
+export LC_NAME=${en_US:-C}
+export LC_ADDRESS=${en_US:-C}
+export LC_TELEPHONE=${en_US:-C}
+export LC_MEASUREMENT=${en_US:-C}
+export LC_IDENTIFICATION=${en_US:-C}
+
+unset pl_PL en_US
+
