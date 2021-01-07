5015b27731b11db8bd3798f9e75bc52bc0716d5c
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-01-07 13:26:28 +0100

    Fix dircolors that has no globbing

--- etc/profile.d/dircolors.sh
+++ etc/profile.d/dircolors.sh
@@ -1,5 +1,15 @@
 
-if hash dircolors 2>/dev/null >&1 && [[ -r "$KCDIR"/etc/dircolors ]]; then
-	eval "$(dircolors -b "$KCDIR"/etc/dircolors)"
+# Older dircolors doesn't support properly TERM glob mathing from configuration
+# So I'll match it myself with case.
+
+if
+	hash dircolors 2>/dev/null >&1 &&
+	[ -r "$KCDIR"/etc/dircolors ] &&
+	case "$TERM" in
+	Eterm|ansi|*color*|con[0-9]*x[0-9]*|cons25|console|cygwin|dtterm|gnome|hurd|jfbterm|konsole|kterm|linux|linux-c|mlterm|putty|rxvt*|screen*|st|terminator|tmux*|vt100|xterm*) true; ;;
+	*) false; ;;
+	esac
+then
+	eval "$(envsubst <"$KCDIR"/etc/dircolors | dircolors -b -)"
 fi
 
