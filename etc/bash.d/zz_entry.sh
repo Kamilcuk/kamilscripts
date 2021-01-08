13d8d2215d50a9cbb32882a17d23bbb69b9cf9be
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-01-08 20:26:15 +0100

    Show tmux and screen detaches

--- etc/bash.d/zz_entry.sh
+++ etc/bash.d/zz_entry.sh
@@ -1,18 +1,26 @@
 
 if [[ $- != *i* ]]; then return; fi
-# if connected via ssh, be empty
-if [[ -n "${SSH_CLIENT:-}" || -n "${SSH_TTY:-}" || -n "${SSH_CONNECTION:-}" ]]; then return; fi
 
 # https://stackoverflow.com/questions/4261876/check-if-bash-script-was-invoked-from-a-shell-or-another-script-application
-# do not show calendar when su-ing
-if ! [[ $(ps -o stat= -p $$) =~ s && -t 0 ]]
-then return; fi
+# If connected with sudo or as child shell.
+if ! [[ $(ps -o stat= -p $$) =~ s && -t 0 ]]; then return; fi
 
-# Put your local functions and aliases herea
-if hash cal 2>/dev/null; then
-	if hash ,color 2>/dev/null; then ,color green; fi
-	cal -3m
-	if hash ,color 2>/dev/null; then ,color reset; fi
+# If not connected via ssh.
+if [[ -z "${SSH_CLIENT:-}" && -z "${SSH_TTY:-}" && -z "${SSH_CONNECTION:-}" ]]; then
+	# Show calendar and a fortune.
+	if hash cal 2>/dev/null; then
+		if hash ,color 2>/dev/null; then ,color green; fi
+		cal -3m
+		if hash ,color 2>/dev/null; then ,color reset; fi
+	fi
+	if hash fortune 2>/dev/null; then fortune; fi
 fi
-if hash fortune 2>/dev/null; then fortune; fi
+
+# Show screen and tmux detached sessions.
+if hash screen 2>/dev/null; then
+	screen -list | sed '/Detached/!d; s/^/screen: /'
+fi
+if hash tmux 2>/dev/null; then
+	tmux ls 2>/dev/null | sed '/(attached)/d; s/$/ (detached)/; s/^/tmux: /'
+fi
 
