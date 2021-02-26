5e9e8c82885291b4ce2ffc7b4a46545b4de951bf
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-02-26 01:26:11 +0100

    try_autorun: Fixes, but don't create stupid a.out files in home dir!

--- bin/,try_autorun.sh
+++ bin/,try_autorun.sh
@@ -4,9 +4,16 @@ set -euo pipefail
 name=$(basename "$0")
 
 run() {
-	echo "+" "$@" >&2
-	if ! "$dry_run"; then
+	printf "+ %s\n" "$*" >&2
+	if ! "$g_dryrun"; then
 		"$@"
+	fi
+}
+
+runeval() {
+	printf "+ %s\n" "$*" >&2
+	if ! "$g_dryrun"; then
+		eval "$1"
 	fi
 }
 
@@ -47,19 +54,17 @@ EOF
 }
 
 parse_arguments() {
-	declare -g file use_project dry_run sync_output
-	use_project=false
-	dry_run=false
-	file=""
-	sync_output=false
+	declare -g file g_use_project g_dryrun g_sync_output
+	g_use_project=false
+	g_dryrun=false
 
 	local o
 	while getopts ":e:pSnh" o; do
 		case "$o" in
 		e) ;;
-		p) use_project=true; ;;
-		S) sync_output=true; ;;
-		n) dry_run=true; ;;
+		p) g_use_project=true; ;;
+		S) exec 2>&1; ;;
+		n) g_dryrun=true; ;;
 		h) usage; exit 0; ;;
 		*) fatal "Invalid argument: -$OPTARG"; ;;
 		esac
@@ -72,12 +77,14 @@ gcc_version() {
 	fi
 }
 
-gcc_try_gcc_compile() {
-	tmpf=$(mktemp --tmpdir tmp.try_autorun.XXXXXXXXXX.c)
-	trap 'rm "$tmpf"' EXIT
-	echo 'int main() { return 0; }' > "$tmpf"
-	LC_ALL=C gcc "${cflags[@]}" "${sanitize[@]}" "$tmpf" </dev/null
-}
+gcc_try_gcc_compile() (
+	local tmps tmpo
+	tmps=$(mktemp --tmpdir tmp.try_autorun.XXXXXXXXXX.c)
+	tmpo=$(mktemp --tmpdir tmp.try_autorun.XXXXXXXXXX.out)
+	trap 'rm "$tmps" "$tmpo"' EXIT
+	echo 'int main(){}' > "$tmps"
+	LC_ALL=C gcc "$@" "$tmps" -o "$tmpo" </dev/null
+)
 
 awkeval() {
 	if (($# != 1)); then fatal "internal error: awkeval takes one argument"; fi
@@ -92,7 +99,6 @@ cflags_detect() {
 	local base
 	base=(
 		-Wall
-		-Wextra
 		-ggdb3
 	)
 	# global cflags
@@ -119,33 +125,41 @@ cflags_detect() {
 	fi
 }
 
-# main #########################################
+list_functions_prefixed() {
+	declare -F | sed "/^declare -f $1/!d; s///"
+}
 
-be_nice "$@"
+,project_detect_make() {
+	[[ -e 'Makefile' ]]
+}
 
-parse_arguments "$@"
-shift "$((OPTIND-1))"
-if (($# != 1)); then usage; fatal "Invalid count of arguments"; fi
-file=$1
-shift
+,project_run_make() {
+	cflags_detect
+	export CFLAGS="${cflags[*]}"
+	export CXXFLAGS="${cflags[*]}"
+	run make "$@"
+}
 
-if "$use_project"; then
-	if [[ -e 'Makefile' ]]; then
-		cflags_detect
-		export CFLAGS="${cflags[*]}"
-		export CXXFLAGS="${cflags[*]}"
-		run make "$@"
-		exit
-	elif [[ -e 'CMakeLists.txt' ]] &&
-			grep -q 'cmake_minimum_required\s*(\s*VERSION.*)' 'CMakeLists.txt'; then
+,project_detect_cmake() {
+	[[ -e 'CMakeLists.txt' ]] &&
+	grep -q 'cmake_minimum_required\s*(\s*VERSION.*)' 'CMakeLists.txt'
+}
+
+,project_run_cmake() {
+	local cdir tmp
+	# Find CMakeCache.txt
+	if
+		tmp=$(timeout 1 find . -mindepth 1 -maxdepth 3 -type f -readable -name 'CMakeCache.txt' -print -quit) &&
+		[[ -r "$tmp" ]]
+	then
+		cdir=${tmp%/*}
+	else
+		cdir=_build
+		local args
 		args=()
-		if hash ninja 2>/dev/null; then args+=(-G Ninja); fi
+		if hash ninja 2>/dev/null; then args+=(-GNinja); fi
 		cflags_detect
-
-		if [[ -e "_build" && ! -e "_build/CMakeCache.txt" ]]; then
-			fatal "cmake _build directory exists and is not cmake build dir. Bailing out"
-		fi
-		run cmake -S. -B_build "${args[@]}" \
+		run cmake -S. -B"$cdir" \
 			-DCMAKE_VERBOSE_MAKEFILE=yes \
 			-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
 			-DCMAKE_C_FLAGS="${cflags[*]}" \
@@ -153,12 +167,32 @@ if "$use_project"; then
 			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY="$PWD"/_build/bin \
 			-DCMAKE_LIBRARY_OUTPUT_DIRECTORY="$PWD"/_build/lib \
 			-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="$PWD"/_build/lib \
-			"$@" \
-			&&
-		run cmake --build _build &&
-		( cd _build && run ctest -V )
-		exit
+			"${args[@]}"
+	fi &&
+	run cmake --build "$cdir" &&
+	if [[ -e "$cdir"/CTestTestfile.cmake ]]; then
+		( runeval "cd $(printf "%q" "$cdir") && ctest" )
 	fi
+}
+
+# main #########################################
+
+be_nice "$@"
+
+parse_arguments "$@"
+shift "$((OPTIND-1))"
+if (($# != 1)); then usage; fatal "Invalid count of arguments: $#"; fi
+file=$1
+shift
+
+
+if "$g_use_project"; then
+	for i in $(list_functions_prefixed ,project_detect_); do
+		if ,project_detect_"$i"; then
+			,project_run_"$i" "$file"
+			exit
+		fi
+	done
 fi
 
 ext=${file,,}
@@ -176,9 +210,6 @@ cpp|cxx|cc|c++)
 	tmp=$(mktemp --suffix='.out')
 	trap 'rm "$tmp"' EXIT
 	{
-		if "$sync_output"; then
-			exec 1>&2
-		fi
 		echo "+" "$ccompiler" "${cflags[@]}" "$file" >&2
 		"$ccompiler" "${cflags[@]}" "$@" -o "$tmp" "$file"
 		"$tmp"
@@ -191,7 +222,7 @@ py)
 sh|bash|bashrc)
 	run bash "$@" "$file"
 	;;
-*) 
+*)
 	fatal "Don't know how to build $file"
 esac
 
