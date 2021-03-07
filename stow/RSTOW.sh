e17da1037f2d8825acaabab0e927b3d6318be4d2
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-03-07 02:45:26 +0100

    Uninstall fix_term_italic

--- stow/RSTOW.sh
+++ stow/RSTOW.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-shopt -s extglob
-PACKAGES=($(
+get_packages() {
+	shopt -s extglob
 	echo common
 	if [[ -e "$HOSTNAME" ]]; then
 		echo "$HOSTNAME"
 	fi
-	if grep -Eqx 'gucio|(dudek|dzik|jenot|kumak|leszcz|wilga|bocian).cis.gov.pl' <<<"$HOSTNAME"; then
+	if grep -Eqx 'gucio|(dudek|dzik|jenot|kumak|leszcz|wilga|bocian).cis.gov.pl' <<<"$HOSTNAME" &&
+			[[ "$g_mode" == "delete" || "$g_mode" == "restow" ]]; then
+		# uninstall it
 		echo fix_term_missing_italic
+
 	fi
-) )
+}
+PACKAGES=($(get_packages))
 
