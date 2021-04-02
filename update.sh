bdc05ae6109e7146051f8d971c45cb3afe05ac2d
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-04-02 13:29:11 +0200

    update.sh detect autostash

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 log "Updating repository..."
 git_remote_get-url | sed_remote_to_https | xargs -t git remote set-url origin
-runlog git pull --rebase --autostash
+autostash=$(git --version | awk '{exit !(0+$3>2.6)}' && printf "%s\n" --autostash)
+runlog git pull --rebase $autostash
 git_remote_get-url | sed_remote_to_ssh | xargs -t git remote set-url origin
 
 log "Updating submodules..."
