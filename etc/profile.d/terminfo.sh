57788369fdc690320f4ead614869353833de9a3e
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-06-03 00:02:08 +0200

    Updates = sshload implement

--- etc/profile.d/terminfo.sh
+++ etc/profile.d/terminfo.sh
@@ -1,24 +1,25 @@
 
 # https://rsapkf.xyz/blog/enabling-italics-vim-tmux
-if hash tic 2>/dev/null >/dev/null; then
-	{
-	if { infocmp xterm+256color; } 2>/dev/null >&2; then cat <<EOF
+if hash tic 2>/dev/null >&2 && hash infocmp 2>/dev/null >&2; then
+	(
+	ihas() { { infocmp "$1"; } 2>/dev/null >&2; }
+	if ihas xterm+256color && ! ihas xterm-256color-italic; then cat <<EOF
 xterm-256color-italic|xterm with 256 colors and italic,
     sitm=\E[3m, ritm=\E[23m,
     use=xterm-256color,
 EOF
 	fi
-	if { infocmp xterm+tmux; } 2>/dev/null >&2; then cat <<EOF
+	if ihas xterm+tmux && ihas screen && ! ihas tmux; then cat <<EOF
 tmux|tmux terminal multiplexer,
     ritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, Ms@,
     use=xterm+tmux, use=screen,
 EOF
 	fi
-	if { infocmp xterm+256setaf; } 2>/dev/null >&2; then cat <<EOF
+	if ihas xterm+256setaf && ihas tmux && ! ihas tmux-256color; then cat <<EOF
 tmux-256color|tmux with 256 colors,
 	use=xterm+256setaf, use=tmux,
 EOF
-	fi; } |
+	fi; ) |
 	tic -x -
 fi
 
