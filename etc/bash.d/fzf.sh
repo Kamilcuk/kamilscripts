c4bca8722a7302294c65dcee563ca1141475ebad
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-07-08 19:58:47 +0200

    updates

--- etc/bash.d/fzf.sh
+++ etc/bash.d/fzf.sh
@@ -1,5 +1,6 @@
 
-if [[ -e /usr/share/fzf/key-bindings.bash ]]; then
-	. /usr/share/fzf/key-bindings.bash
+if command -v fzf-share >/dev/null; then
+  source "$(fzf-share)/key-bindings.bash"
+  source "$(fzf-share)/completion.bash"
 fi
 
