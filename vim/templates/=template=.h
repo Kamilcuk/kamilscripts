ade1f20c8657d7c1f1d180c8c6033069d5f77c7b
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-08-30 12:29:07 +0200

    Updates

--- vim/templates/=template=.h
+++ vim/templates/=template=.h
@@ -2,11 +2,17 @@
  * @file
  * @author Kamil Cukrowski <%MAIL%>
  * @date %YEAR%-%MONTH%-%DAY%
- * SPDX-License-Identifier:
+ * SPDX-License-Identifier: MIT AND Beerware
  */
 #ifndef %GUARDPATH%
 #define %GUARDPATH%
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 %HERE%
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* %GUARDPATH% */
