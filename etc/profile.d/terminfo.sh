9c18715b5dcc13d56245179f51b1a64a4c052d71
Kamil Cukrowski <kamilcukrowski@gmail.com>
2022-01-22 19:47:00 +0100

    Disable xterm356-italic

--- etc/profile.d/terminfo.sh
+++ etc/profile.d/terminfo.sh
@@ -1,26 +1,26 @@
 
 # https://rsapkf.xyz/blog/enabling-italics-vim-tmux
-if hash tic 2>/dev/null >&2 && hash infocmp 2>/dev/null >&2; then
-	(
-	ihas() { { infocmp "$1"; } 2>/dev/null >&2; }
-	if ihas xterm+256color && ! ihas xterm-256color-italic; then cat <<EOF
-xterm-256color-italic|xterm with 256 colors and italic,
-    sitm=\E[3m, ritm=\E[23m,
-    use=xterm-256color,
-EOF
-	fi
-	if ihas xterm+tmux && ihas screen && ! ihas tmux; then cat <<EOF
-tmux|tmux terminal multiplexer,
-    ritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, Ms@,
-    use=xterm+tmux, use=screen,
-EOF
-	fi
-	if ihas xterm+256setaf && ihas tmux && ! ihas tmux-256color; then cat <<EOF
-tmux-256color|tmux with 256 colors,
-	use=xterm+256setaf, use=tmux,
-EOF
-	fi; ) |
-	tic -x -
-fi
+#if hash tic 2>/dev/null >&2 && hash infocmp 2>/dev/null >&2; then
+	#(
+	#ihas() { { infocmp "$1"; } 2>/dev/null >&2; }
+	#if ihas xterm+256color && ! ihas xterm-256color-italic; then cat <<EOF
+#xterm-256color-italic|xterm with 256 colors and italic,
+    #sitm=\E[3m, ritm=\E[23m,
+    #use=xterm-256color,
+#EOF
+	#fi
+	#if ihas xterm+tmux && ihas screen && ! ihas tmux; then cat <<EOF
+#tmux|tmux terminal multiplexer,
+    #ritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, Ms@,
+    #use=xterm+tmux, use=screen,
+#EOF
+	#fi
+	#if ihas xterm+256setaf && ihas tmux && ! ihas tmux-256color; then cat <<EOF
+#tmux-256color|tmux with 256 colors,
+	#use=xterm+256setaf, use=tmux,
+#EOF
+	#fi; ) |
+	#tic -x -
+#fi
 
 
