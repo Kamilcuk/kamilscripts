b91e8c6e76d49f8b480fac34131b995637baedae
Kamil Cukrowski <kamilcukrowski@gmail.com>
2023-02-09 15:20:22 +0100

    commit from leonidas

--- etc/profile.d/bc.sh
+++ etc/profile.d/bc.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 if [[ $- != *i* ]]; then return; fi
 
@@ -8,7 +9,9 @@ fi
 
 # https://unix.stackexchange.com/questions/609618/is-it-possible-to-save-bc-command-line-history/609700#609700
 if hash rlwrap 2>/dev/null; then
-	bc() { rlwrap -a -H "$HOME"/.cache/bc_history bc "$@"; }
+	bc() { LC_ALL=C rlwrap -a -H "$HOME"/.cache/bc_history bc "$@"; }
+else
+	bc() { LC_ALL=C command bc "$@"; }
 fi
 # https://stackoverflow.com/questions/22621488/is-there-an-rc-file-for-the-command-line-calculator-bc
 BC_ENV_ARGS=" -q -l $(echo "$KCDIR"/etc/bc.d/*.bc)"
