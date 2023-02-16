9ecdadf9860d58bbf1de0fdfe33c219ef8effbd3
Kamil Cukrowski <kamilcukrowski@gmail.com>
2023-02-16 01:25:33 +0100

    Updates from rpi3

--- autocommit.sh
+++ autocommit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -euo pipefail
-git fetch
-branch=$(git rev-parse --abbrev-ref HEAD)
-git merge-base --is-ancestor origin/master master
+set -xeuo pipefail
+git add -A
+git commit -m "Updates from $HOSTNAME"
+git push
 
