222c2142b310d1b22b5c0df9e7f3e48f7794e0fa
Kamil Cukrowski <kamilcukrowski@gmail.com>
2023-02-25 14:08:27 +0100

    add chrootroot

--- etc/profile.d/bc.sh
+++ etc/profile.d/bc.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-if [[ $- != *i* ]]; then return; fi
-
-if [[ -z "$KCDIR" ]]; then
-	echo "bc.sh: KCDIR not found" >&2
-	return
-fi
+case "$-" in *i*) ;; *) return ;; esac
 
 # https://unix.stackexchange.com/questions/609618/is-it-possible-to-save-bc-command-line-history/609700#609700
-if hash rlwrap 2>/dev/null; then
-	bc() { LC_ALL=C rlwrap -a -H "$HOME"/.cache/bc_history bc "$@"; }
-else
-	bc() { LC_ALL=C command bc "$@"; }
-fi
-# https://stackoverflow.com/questions/22621488/is-there-an-rc-file-for-the-command-line-calculator-bc
-BC_ENV_ARGS=" -q -l $(echo "$KCDIR"/etc/bc.d/*.bc)"
-export BC_ENV_ARGS
+bc() {
+	# https://stackoverflow.com/questions/22621488/is-there-an-rc-file-for-the-command-line-calculator-bc
+	if [ -d "${KCDIR:-}" ]; then
+		BC_ENV_ARGS=" -q -l $(echo "$KCDIR"/etc/bc.d/*.bc)"
+	fi
+	if hash rlwrap 2>/dev/null; then
+		BC_ENV_ARGS="$BC_ENV_ARGS" LC_ALL=C rlwrap -a -H "$HOME"/.cache/bc_history bc "$@"
+	else
+		BC_ENV_ARGS="$BC_ENV_ARGS" LC_ALL=C command bc "$@"
+	fi
+}
 
