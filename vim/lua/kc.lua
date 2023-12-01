c44afd732b879424281c2549d3b3531ad37740a3
Kamil Cukrowski <kamilcukrowski@gmail.com>
2023-12-01 09:59:44 +0100

    leonidas: Updates

--- vim/lua/kc.lua
+++ vim/lua/kc.lua
@@ -1,135 +1,173 @@
 -- kc.lua
+-- local vim = require('vim')
 
 ---@param cmd string
 ---@return boolean
 local function hascmd(cmd)
-    return vim.fn.executable(cmd)
+	return vim.fn.executable(cmd)
 end
 
 ---@param cmd string
 ---@param exe fun() -> nil|string
 ---@return boolean
 local function doifnocmd(cmd, exe)
-    if not vim.fn.executable("black") then
-        if type(exe) == "function" then
-            exe()
-        elseif type(exe) == "string" then
-            print("Executing " .. exe)
-            vim.cmd(exe)
-        else
-            assert(false, "Type(exe)=" .. type(exe))
-        end
-        return true
-    end
-    return false
+	if not vim.fn.executable("black") then
+		if type(exe) == "function" then
+			exe()
+		elseif type(exe) == "string" then
+			print("Executing " .. exe)
+			vim.cmd(exe)
+		else
+			assert(false, "Type(exe)=" .. type(exe))
+		end
+		return true
+	end
+	return false
+end
+
+local function lcmd(what)
+	print(what)
+	vim.cmd(what)
+end
+
+local function execute(what)
+	print("!" .. what)
+	os.execute(what)
 end
 
 ---@param what string
 function CocInstall(what)
-    if vim.fn.exists("CocInstall") then
-        vim.cmd(":CocInstall " .. what)
-    end
+	if vim.fn.exists(":CocInstall") then
+		lcmd(":CocInstall " .. what)
+	end
 end
 
 ---@param what string
-function npm_install(what)
-    vim.cmd("!npm install -g " .. what)
+local function npm_install(what)
+	vim.cmd("!npm install -g " .. what)
+end
+
+-- Run TSInstall
+---@param what string
+local function TSInstall(what)
+	if vim.fn.exists(":TSInstallSync") and vim.fn.exists(":TSUpdate") then
+		if pcall(vim.treesitter.language.inspect, what) then
+			lcmd(":TSUpdate " .. what)
+		else
+			lcmd(":TSInstall " .. what)
+		end
+	end
+end
+
+---@param list string[]
+local function parallelcmd(list)
+	local cmd = ""
+	cmd = ""
+	for _, i in ipairs(list) do
+		cmd = cmd .. " '" .. i:gsub("^!", ""):gsub("'", "\\'") .. "'"
+	end
+	vim.cmd("!printf \\%s\\\\n " .. cmd .. " | xargs -P$(nproc) -i bash -xc {}")
 end
 
 -------------------------------------------------------------------------------
 
----@class Lang
 local Lang = {}
 
 function Lang.nomad()
-    vim.cmd("!pipx install nomad-watch")
+	vim.cmd("!pipx install nomad-watch")
 end
 
 function Lang.python()
-    vim.cmd("!pip install --upgrade pynvim")
-    vim.cmd("!pipx install black")
-    vim.cmd("!pipx install isort")
-    vim.cmd("!pipx install pyflyby")
-    vim.cmd("!pipx runpip pyflyby install --upgrade black")
-    npm_install("pyright")
-    CocInstall("coc-pyright coc-yaml")
+	TSInstall("python")
+	parallelcmd({
+		"pip install --upgrade pynvim",
+		"pipx install --force black",
+		"pipx install --force isort",
+		"pipx install --force pyflyby && pipx runpip pyflyby install --upgrade black",
+		"pipx install --force autoimport",
+		"pipx install --force pylava && pipx runpip pylava install pyflakes==2.4.0",
+		"npm install -g pyright",
+	})
+	CocInstall("coc-pyright coc-yaml")
 end
 
 function Lang.yaml()
-    CocInstall("coc-yaml")
+	CocInstall("coc-yaml")
 end
 
 function Lang.lua()
-    CocInstall("coc-lua")
-    doifnocmd(
-        "stylua",
-        function()
-            print("Install cargo and then cargo install stylua")
-        end
-    )
+	TSInstall("lua")
+	npm_install("lua-fmt")
+	doifnocmd("stylua", function()
+		print("Install cargo and then cargo install stylua")
+	end)
+	CocInstall("coc-lua")
 end
 
 function Lang.vim()
-    CocInstall("coc-vimlsp")
+	CocInstall("coc-vimlsp")
+	TSInstall("vim")
 end
 
 function Lang.groovy()
-    CocInstall("coc-groovy")
+	CocInstall("coc-groovy")
+	TSInstall("groovy")
 end
 
 function Lang.c()
-    CocInstall("coc-clangd coc-cmake")
+	CocInstall("coc-clangd coc-cmake")
+	TSInstall("c")
 end
 
 function Lang.cmake()
-    Lang.c()
+	Lang.c()
+	TSInstall("cmake")
 end
 
 function Lang.cpp()
-    Lang.c()
+	Lang.c()
+	TSInstall("cpp")
 end
 
 function Lang.tex()
-    CocInstall("coc-vimtext")
+	CocInstall("coc-vimtext")
+	TSInstall("tex")
 end
 
 function Lang.ruby()
-    CocInstall("coc-solargraph")
+	CocInstall("coc-solargraph")
+	TSInstall("ruby")
 end
 
 function Lang.markdown()
-    npm_install("markdownlint --save-dev")
+	npm_install("markdownlint --save-dev")
+	TSInstall("markdown")
 end
 
 function Lang.perl()
-    -- vim.cmd("!cpan Perl::LangugeServer")
-    npm_install("perlnavigator-server")
-    CocInstall("coc-perl")
-end
-
-function Lang.lua()
-    npm_install("lua-fmt")
-    CocInstall("coc-lua")
+	-- vim.cmd("!cpan Perl::LangugeServer")
+	npm_install("perlnavigator-server")
+	CocInstall("coc-perl")
+	TSInstall("perl")
 end
 
 -------------------------------------------------------------------------------
 
 local kc = {}
 
-function kc.lang(args)
-    print(vim.inspect(args))
-    local filetype = args and args.fargs[1] or vim.bo.filetype
-    assert(filetype ~= nil and filetype ~= "", "filetype = " .. vim.inspect(filetype))
-    print("Setuping " .. vim.inspect(filetype))
-    local func = Lang[filetype]
-    if func == nil then
-        print("Configuration for " .. filetype .. " not found")
-        return nil
-    end
-    return func()
+function kc.lang(arg)
+	print(vim.inspect(arg))
+	local filetype = arg and arg or vim.bo.filetype
+	assert(filetype ~= nil and filetype ~= "", "filetype = " .. vim.inspect(filetype))
+	print("Setuping " .. vim.inspect(filetype))
+	local func = Lang[filetype]
+	if func == nil then
+		print("Configuration for " .. filetype .. " not found")
+		return nil
+	end
+	return func()
 end
 
-function kc.setup()
-end
+function kc.setup() end
 
 return kc
