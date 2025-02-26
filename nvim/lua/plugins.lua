206d1cbe19c75960ecbb6da535f21c3e2358e609
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-02-26 22:40:31 +0100

    up

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -48,6 +48,11 @@ local function KcLog(data)
   end
 end
 
+local function KcEnableAtHome()
+  -- if user is cukrowsk
+  return vim.fn.getenv "USER" == "cukrowsk"
+end
+
 ---@param timeout_s number
 ---@param start fun(): any
 ---@param stop fun(any): nil
@@ -563,6 +568,8 @@ return {
   {
     -- "HampusHauffman/bionic.nvim",
     "kamilcuk/bionic.nvim",
+    -- This plugin causes HIGH slow down when passing code from clipboard, disabling it solves the problem.
+    enabled = false,
     branch = "fix-index-nil-value",
     dependencies = {
       "AstroNvim/astrocore",
@@ -1126,7 +1133,8 @@ p                paste yanked block replace with current selection
       opts.highlight.disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then return true end
+        return false
+        -- return ok and stats and stats.size > max_filesize
       end
     end,
   },
@@ -1196,5 +1204,58 @@ p                paste yanked block replace with current selection
       dismiss_keymap = nil,
     },
   },
+
+  {
+    "yetone/avante.nvim",
+    enabled = KcEnableAtHome(),
+    event = "VeryLazy",
+    lazy = false,
+    version = "*", -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+      provider = "copilot",
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        enabled = false,
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+
   -- }}}
 }
